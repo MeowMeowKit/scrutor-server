1284e05496f91203ec465cafcd3ee1868ace3489
Duy Huynh Quoc <duyhq.zee@gmail.com>
2022-06-22 14:22:20 +0700

    [New] ClassAPI

---
+++ src/main/java/api/ClassAPI.java
@@ -0,0 +1,19 @@
+package api;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+//@WebServlet(name = "ClassAPI", value = "/ClassAPI")
+public class ClassAPI extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+}
