6a51eb9ecd92b85a61512e0305d23d93cb094228
MeowMeowKit <92076631+MeowMeowKit@users.noreply.github.com>
2022-06-30 09:57:14 +0700

    Update classDAO

--- src/main/java/daos/ClassDAO.java
+++ src/main/java/daos/ClassDAO.java
@@ -1,18 +1,19 @@
 package daos;
 
-import utils.DBUtils;
+import java.util.ArrayList;
 import dtos.Class;
-
+import dtos.User;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
+import java.sql.SQLException;
+import utils.DBUtils;
 
 public class ClassDAO {
 
-    private static Connection conn;
-    private static PreparedStatement preStm;
-    private static ResultSet rs;
+    private static Connection conn = null;
+    private static PreparedStatement preStm = null;
+    private static ResultSet rs = null;
 
     private static void closeConnection() {
         try {
@@ -33,20 +34,49 @@ public class ClassDAO {
 
     // Get all the classes that are taught by teacher A
     public static ArrayList<Class> getClassesByTeacherId(String teacherId) {
+        conn = null;
+        preStm = null;
+        rs = null;
         ArrayList<Class> list = new ArrayList<>();
+        ArrayList<User> listUser = new ArrayList<>();
+
         try {
             conn = DBUtils.makeConnection();
-            String sql = "SELECT classID, name  "
-                    + "FROM Class "
-                    + "WHERE teacherId = ? ";
+            String sql = "SELECT u.userId, u.fullName, u.email, u.password, u.role, cl.classId, cl.name, cl.teacherId  "
+                    + "FROM (User u right join User_Class ucl on u.userId = ucl.userId) left join Class cl on ucl.classId = cl.classId "
+                    + "WHERE cl.teacherId = ? AND u.role = 'student' "
+                    + "ORDER BY cl.classId";
             preStm = conn.prepareStatement(sql);
             preStm.setString(1, teacherId);
             rs = preStm.executeQuery();
-            while (rs.next()) {
-                String classID = rs.getString("classID");
-                String name = rs.getString("name");
-                list.add(new Class(classID, teacherId, name));
+            
+            if (rs.next()) {
+                //Tạo biến chứa giá trị classId của row hiện tại
+                String classId = rs.getString("cl.classId");
+                while (rs.next()) {
+                    //Create a Student list
+                    String userId = rs.getString("u.userId");
+                    String fullName = rs.getString("u.fullName");
+                    String email = rs.getString("u.email");
+                    String password = rs.getString(" u.password");
+                    String role = rs.getString("u.role");
+                    
+                    //Phân loại lớp
+                    String newClassId = rs.getString("cl.classId");
+                    //So sánh row đang xét có trùng classId với row trên không
+                    if(newClassId != classId){
+                        classId = newClassId;
+                        list.add(new Class(rs.getString("cl.classID"), teacherId, rs.getString("cl.name"), listUser));
+                        
+                        //Add vô Student đầu tiên vô class mới
+                        listUser = null;
+                        listUser.add(new User(userId, fullName, email, password, role));
+                    }else{
+                        listUser.add(new User(userId, fullName, email, password, role));
+                    }
+                }
             }
+            return list;
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -57,20 +87,50 @@ public class ClassDAO {
 
     // Get all the classes that student A attends
     public static ArrayList<Class> getClassesByStudentId(String studentId) {
+        conn = null;
+        preStm = null;
+        rs = null;
         ArrayList<Class> list = new ArrayList<>();
+        ArrayList<User> listUser = new ArrayList<>();
+
         try {
             conn = DBUtils.makeConnection();
-            String sql = "SELECT cl.classId, cl.teacheId, cl.name  "
-                    + "FROM Class cl left join User_Class ucl on cl.classId = ucl.classId "
-                    + "WHERE ucl.userId = ? ";
+            String sql = "SELECT cl.classId, cl.teacheId, cl.name , u.userId, u.fullName, u.email, u.password, u.role"
+                    + "FROM ( Class cl left join User_Class ucl on cl.classId = ucl.classId ) inner join User u on ucl.userId = u.userId  "
+                    + "WHERE ucl.userId = ? AND u.role = 'student'  "
+                    + "ORDER BY cl.classId";
             preStm = conn.prepareStatement(sql);
             preStm.setString(1, studentId);
             rs = preStm.executeQuery();
-            while (rs.next()) {
-                String classID = rs.getString("cl.classID");
-                String name = rs.getString("cl.name");
-                list.add(new Class(classID, studentId, name));
+    
+            if (rs.next()) {
+                //Tạo biến chứa giá trị classId của row hiện tại
+                String classId = rs.getString("cl.classId");
+                while (rs.next()) {
+                    //Create a Student list
+                    String userId = rs.getString("u.userId");
+                    String fullName = rs.getString("u.fullName");
+                    String email = rs.getString("u.email");
+                    String password = rs.getString(" u.password");
+                    String role = rs.getString("u.role");
+                  
+                    
+                     //Phân loại lớp
+                    String newClassId = rs.getString("cl.classId");
+                    //So sánh row đang xét có trùng classId với row trên không
+                    if(newClassId != classId){
+                        classId = newClassId;
+                        list.add(new Class(rs.getString("cl.classID"), rs.getString("cl.teacheId"), rs.getString("cl.name"), listUser));
+                        
+                        //Add vô Student đầu tiên vô class mới
+                        listUser = null;
+                        listUser.add(new User(userId, fullName, email, password, role));
+                    }else{
+                        listUser.add(new User(userId, fullName, email, password, role));
+                    }  
+                }
             }
+            return list;
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -80,23 +140,35 @@ public class ClassDAO {
     }
 
     // Create a new class that taught by teacher A
-    public static Class createNewClass(Class c, String teacherId) {
+    public static Class createNewClass(Class c, String teacherId) throws SQLException {
+        conn = null;
+        preStm = null;
+        rs = null;
         Class classResult = null;
+        ArrayList<User> listUser = new ArrayList<>();
         try {
+            //Create a Class
             conn = DBUtils.makeConnection();
             String sql = "INSERT INTO Class "
-                    + "Values( ?, ?, ?)";
+                    + "VAVLUES( ?, ?, ?)";
             preStm = conn.prepareStatement(sql);
             preStm.setString(1, c.getClassId());
             preStm.setString(2, teacherId);
             preStm.setString(3, c.getName());
-
             rs = preStm.executeQuery();
-            while (rs.next()) {
-                String classID = rs.getString("classID");
-                String name = rs.getString("name");
-                classResult = new Class(classID, teacherId, name);
+
+            //Insert The Student into class
+            preStm = null;
+            rs = null;
+            for (User user : c.getStudents()) {
+                sql = "INSERT INTO USER_CLASS "
+                        + "VALUES(? , ?)";
+                preStm = conn.prepareStatement(sql);
+                preStm.setString(1, user.getUserId());
+                preStm.setString(2, c.getClassId());
+                listUser.add(new User(user.getUserId(), user.getFullName(), user.getEmail(), user.getPassword(), user.getRole()));
             }
+            classResult = new Class(c.getClassId(), teacherId, c.getName(), listUser);
             return classResult;
         } catch (Exception e) {
             e.printStackTrace();
@@ -107,8 +179,11 @@ public class ClassDAO {
     }
 
     // A student attends a new class
-    public static boolean attendNewClass(Class c, String studentId) {
-        boolean result = false;
+    public static int attendNewClass(Class c, String studentId) {
+        conn = null;
+        preStm = null;
+        rs = null;
+        int result = 0;// Nếu Hàm trả về 0 thì sẽ không có dòng nào ảnh hưởng
         try {
             conn = DBUtils.makeConnection();
             String sql = "INSERT INTO User_Class "
@@ -118,10 +193,7 @@ public class ClassDAO {
             preStm.setString(2, c.getClassId());
 
             rs = preStm.executeQuery();
-            int row = preStm.executeUpdate();
-            if (row > 0) {
-                result = true;
-            }
+            result = preStm.executeUpdate();
             return result;
         } catch (Exception e) {
             e.printStackTrace();
@@ -132,20 +204,43 @@ public class ClassDAO {
     }
 
     // Update class (edit className; edit student list add, remove student from class)
-    public static boolean updateClass(Class c) {
-        boolean result = false;
+    public static int updateClass(Class c) {
+        conn = null;
+        preStm = null;
+        rs = null;
+        String sql = null;
+        int result = 0;// Nếu Hàm trả về 0 thì sẽ không có dòng nào ảnh hưởng
         try {
-            String sql = "UPDATE Class "
+
+            //Update the NAME of Class
+            sql = "UPDATE Class "
                     + "SET name = ? "
                     + "WHERE classId = ?";
             preStm = conn.prepareStatement(sql);
             preStm.setString(1, c.getName());
             preStm.setString(2, c.getClassId());
-
             rs = preStm.executeQuery();
-            int row = preStm.executeUpdate();
-            if (row > 0) {
-                result = true;
+
+            //Delete old menbers in a Class
+            preStm = null;
+            rs = null;
+            sql = "DELETE FROM User_Class "
+                    + "Where classId = ? ";
+            preStm = conn.prepareStatement(sql);
+            preStm.setString(1, c.getClassId());
+            rs = preStm.executeQuery();
+
+            //INSERT NEW data in CLASS
+            preStm = null;
+            rs = null;
+            for (User user : c.getStudents()) {
+                sql = "INSERT INTO User_Class "
+                        + "Values( ?, ?) ";
+                preStm = conn.prepareStatement(sql);
+                preStm.setString(1, user.getUserId());
+                preStm.setString(2, c.getClassId());
+                rs = preStm.executeQuery();
+                result = preStm.executeUpdate();
             }
             return result;
         } catch (Exception e) {
@@ -157,19 +252,28 @@ public class ClassDAO {
     }
 
     // A teacher delete a class
-    public static boolean deleteClass(String classId) {
-        boolean result = false;
+    public static int deleteClass(String classId) {
+        conn = null;
+        preStm = null;
+        rs = null;
+        int result = 0; // Nếu Hàm trả về 0 thì sẽ không có dòng nào ảnh hưởng
+        String sql = null;
         try {
             conn = DBUtils.makeConnection();
-            String sql = "DELETE FROM Class "
+            //Delete All Student in Clas
+            sql = "DELETE FROM User_Class "
                     + "WHERE classId = ? ";
             preStm = conn.prepareStatement(sql);
             preStm.setString(1, classId);
             rs = preStm.executeQuery();
-            int row = preStm.executeUpdate();
-            if (row > 0) {
-                result = true;
-            }
+
+            //Delete Class
+            preStm = null;
+            sql = "DELETE FROM Class "
+                    + "WHERE classId = ? ";
+            preStm.setString(1, classId);
+            rs = preStm.executeQuery();
+            result = preStm.executeUpdate();
             return result;
         } catch (Exception e) {
             e.printStackTrace();
@@ -180,21 +284,21 @@ public class ClassDAO {
     }
 
     // A student leave a class
-    public static boolean leaveClass(String userId, String classId) {
-        boolean result = false;
+    public static int leaveClass(String userId, String classId) {
+        conn = null;
+        preStm = null;
+        rs = null;
+        int result = 0; // Nếu Hàm trả về 0 thì sẽ không có dòng nào ảnh hưởng
         try {
             conn = DBUtils.makeConnection();
             String sql = "DELETE FROM User_Class "
-                    + "WHERE userId = ? AND classId = ? ";
+                    + "WHERE userId = ? AND classId = ?";
             preStm = conn.prepareStatement(sql);
             preStm.setString(1, userId);
             preStm.setString(2, classId);
 
             rs = preStm.executeQuery();
-            int row = preStm.executeUpdate();
-            if (row > 0) {
-                result = true;
-            }
+            result = preStm.executeUpdate();
             return result;
         } catch (Exception e) {
             e.printStackTrace();
